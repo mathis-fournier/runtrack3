751b118e9d991d288ac25c11e7005aa2f77f7445
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-08 10:11:04 +0200

    Update index.php

--- job07/index.php
+++ job07/index.php
@@ -7,12 +7,6 @@
     <script src="scripts.js"></script>
 </head>
 <body>
-    <input
-  type="datetime-local"
-  id="meeting-time"
-  name="meeting-time"
-  value="2018-06-12T19:30"
-  min="2020-01-01T00:00"
-  max="2020-31-31T00:00" />
+
 </body>
 </html>
