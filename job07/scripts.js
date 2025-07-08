e05c529cff58848d3a7236a267682319e31b44f3
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-08 10:10:49 +0200

    jourferié

--- job07/scripts.js
+++ job07/scripts.js
@@ -1,7 +1,30 @@
-function jourtravaille(Date){
+function jourtravaille(date){
+    // date = new Date();
+}
+const jourferie = [
+    new Date(Date.UTC(2020, 3, 13)),
+    new Date(Date.UTC(2020, 0, 1))
+]
+
+
+
+var a = new Date(prompt("date 20XX-MM-DD"));
+
+for (var i=0; i < jourferie.length; i++) {
+
+if (a <= jourferie[i]) 
+{
+    console.log("jour ferié");
+} 
+else 
+{
+    console.log("jour travaillé");
+}
 
 }
 
+console.log(a);
+console.log(jourferie); 
 jourtravaille();
 
 
--- job08/scripts.js
+++ job08/scripts.js
@@ -1,8 +1,4 @@
 function nombrepremier () {
 
-}
+}  
 
-var a = parseInt(prompt(""));
-var b = parseInt(prompt(""));
-
-console.log(a+b);
