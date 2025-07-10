8bb6b0a55c1cf65e764b337381a2302c4a7738e0
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 09:24:45 +0200

    citation button

---
+++ jour2/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <article id = citation>"La vie a beaucoup plus d’imagination que nous”.</article>
+    <button id = button>test</button>
+    
+<script src = "scripts.js"></script>
+</body>
+</html>
---
+++ jour2/job01/scripts.js
@@ -0,0 +1,7 @@
+function citation ()
+{
+console.log(citationtext);
+}
+
+const citationtext = document.getElementById("citation").textContent;
+const button = document.getElementById("button").addEventListener("click",citation);
