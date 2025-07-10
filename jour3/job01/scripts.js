5df65d78a78d4597c5752166df61ab4dd74a2b70
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 16:08:45 +0200

    crée jour3+job

---
+++ jour3/job01/scripts.js
@@ -0,0 +1,6 @@
+function citation () {
+  console.log(citationtext)
+}
+
+const citationtext = document.getElementById('citation').textContent
+const button = document.getElementById('button').addEventListener('click', citation)
---
+++ jour3/job03/scripts.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('button').addEventListener('click', addone)
+const compteur = document.getElementById('compteur')
+
+let clicks = 0
+compteur.innerHTML = clicks
+
+function addone () {
+  clicks++
+  compteur.innerHTML = clicks
+}
