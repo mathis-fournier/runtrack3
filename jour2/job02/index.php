6c1d6368ba5e73aa369ccabfebfcbe18e58120f0
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 09:38:45 +0200

    showhide

---
+++ jour2/job02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+
+
+<button onclick ="showhide()"; id = "button">showhide</button>
+<article id = "texte">“L'important n'est pas la chute, mais l'atterrissage.”</article>
+<script src = "scripts.js"></script>
+</body>
+</html>
