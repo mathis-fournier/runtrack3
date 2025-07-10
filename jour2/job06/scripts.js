c5e7671b315d67ad1023ac8779e070a94daf9d75
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-10 11:35:34 +0200

    toggle theme

---
+++ jour2/job06/scripts.js
@@ -0,0 +1,17 @@
+document.getElementById("keylogger").style.display = "none";
+
+
+
+document.addEventListener('keyup', (e) => 
+{
+const textarea = document.getElementById("keylogger");
+textarea.value = textarea.value + e.key;
+console.log(textarea.value); // A SUPPRIMER
+let element = document.body;
+if (textarea.value == "text"){
+    element.classList.toggle("theme");
+    textarea.value = '';
+}
+})
+
+
