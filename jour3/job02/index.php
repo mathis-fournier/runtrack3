56cf9d6bcf2d5f8fb886e1871b293d2c71acc5a3
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-11 14:56:02 +0200

    arc en ciel

--- jour3/job02/index.php
+++ jour3/job02/index.php
@@ -4,19 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src = "scripts.js"></script>
+    <link href="styles.css"rel="stylesheet" />
+
 </head>
 <body>
-    
+
 
 <div id="container">
-<img src="arc1.png" alt="img1">
-<img src="arc2.png" alt="img2">
-<img src="arc3.png" alt="img3">
-<img src="arc4.png" alt="img4">
-<img src="arc5.png" alt="img5">
-<img src="arc6.png" alt="img6">
+    <ul id="list" style="list-style: none; display: inline-flex;">
+<li id="1"><img class="image" id="1img" src="arc1.png" alt="img1"></li>
+<li id="2"><img class="image" id="2img" src="arc2.png" alt="img2"></li>
+<li id="3"><img class="image" id="3img" src="arc3.png" alt="img3"></li>
+<li id="4"><img class="image" id="4img" src="arc4.png" alt="img4"></li>
+<li id="5"><img class="image" id="5img" src="arc5.png" alt="img5"></li>
+<li id="6"><img class="image" id="6img" src="arc6.png" alt="img6"></li>
+    </ul>
 </div>
+
+<button id="gauche"><-</button>
+<button id="droite">-></button>
+
+<button id="melange">Melanger</button>
+<button id="reset">Reset</button>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="scripts.js"></script>
 </body>
 </html>
