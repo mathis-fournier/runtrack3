7160553aebdda5dde1bfed9622d059c3e1054143
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-11 10:02:30 +0200

    .

--- jour3/job01/scripts.js
+++ jour3/job01/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+
   $("#texte").click(function(){
     $("#textep").text("Les logiciels et les cathédrales,c'est un peu la même chose - d'abord on les construit, ensuite on prie.");
   })
@@ -9,6 +10,7 @@ $(document).ready(function()
   {
     $("*").hide();
   });
+
 
 
 });
--- jour3/job02/index.php
+++ jour3/job02/index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src = "scripts.js"></script>
 </head>
 <body>
+    
 
-
-<button onclick ="showhide()"; id = "button">showhide</button>
-<article id = "texte">“L'important n'est pas la chute, mais l'atterrissage.”</article>
-<script src = "scripts.js"></script>
+<div id="container">
+<img src="arc1.png" alt="img1">
+<img src="arc2.png" alt="img2">
+<img src="arc3.png" alt="img3">
+<img src="arc4.png" alt="img4">
+<img src="arc5.png" alt="img5">
+<img src="arc6.png" alt="img6">
+</div>
 </body>
 </html>
