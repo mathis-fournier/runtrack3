12ba9303e22e7dafdf759e96696db9ec7c584190
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-15 11:47:56 +0200

    job02

--- jour04/job01/scripts.js
+++ jour04/job01/scripts.js
@@ -1,4 +1,14 @@
+jQuery(document).ready(function($) {
+   // Votre code ici avec les appels à la fonction $()
+
+
+$("#button").click(function(){
 fetch("expression.txt")
 .then((x) => x.text())
 .then((y) =>
 document.getElementById("p").innerHTML = y)
+});
+
+
+
+});
