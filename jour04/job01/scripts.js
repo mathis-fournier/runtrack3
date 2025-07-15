fd7eca575b6272a61fd55712b352629e8603ad49
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-15 11:21:50 +0200

    cree jour4 job 1-4

---
+++ jour04/job01/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+
+  $("#texte").click(function(){
+    $("#textep").text("Les logiciels et les cathédrales,c'est un peu la même chose - d'abord on les construit, ensuite on prie.");
+  })
+
+
+  $("#hidehtml").click(function()
+  {
+    $("*").hide();
+  });
+
+
+
+});
