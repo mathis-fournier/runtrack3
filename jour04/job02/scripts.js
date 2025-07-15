7c202bac99b79bc11af3f47766a106796dd81fae
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-15 13:02:44 +0200

    Update scripts.js

--- jour04/job02/scripts.js
+++ jour04/job02/scripts.js
@@ -1,3 +1,13 @@
-function jsonValueKey($json, $key){
-
+function jsonValueKey(json, key){
 }
+
+json = 
+{
+name: "La Plateforme_",
+address: "8 rue d'hozier",
+city: "Marseille",
+nb_staff: "11",
+creation:"2019"
+};
+
+console.log(json.name);
