8bf426fcb43651851a9695e4ef643b7e64f8a9ce
mathis-fournier <mathis.fournier@laplateforme.io>
2025-07-17 11:12:49 +0200

    phpmyadmin + mysql / mysqli

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -7,8 +7,42 @@
     
 </head>
 <body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>nom</th>
+        <th>prenom</th>
+        <th>email</th>
+    </tr>
+
+    
+<?php 
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password, 'utilisateurs');
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM utilisateurs";
+$result = $conn->query($sql);
 
 
+if (!$result) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nom"] . "</td><td>" . $row["prenom"] . "</td><td>" . $row["email"] . "</td></tr>";
+    }
+}
+
+?>
+</table>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src = "scripts.js"></script>
 </body>
